9d0f03a2fcf3fc0ec07c4f863b6c08813e46cac1
ningxu <2917720674@qq.com>
2019-08-02 11:20:21 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// @是一个路径的别名 是src路径 在vue-cli项目下有效
+// index 是目录的索引文件，默认使用的索引文件 index.js
+import router from '@/router'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
