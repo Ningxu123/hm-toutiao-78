31d8f9c460aaebeb3b1f736bf1f7957d63868f5c
ningxu <2917720674@qq.com>
2019-08-04 10:04:16 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import 'element-ui/lib/theme-chalk/index.css'
 // @是一个路径的别名 是src路径 在vue-cli项目下有效
 // index 是目录的索引文件，默认使用的索引文件 index.js
 import router from '@/router'
+// 导入样式
+import '@/styles/index.less'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,95 @@
 <template>
-  <div class="container">Login</div>
+  <div class="container">
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt="">
+      <!-- 表单 -->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+            <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    // 自定义校验函数
+    const checkMobile = (rule, value, callback) => {
+      // 按照自己的校验逻辑去校验value即可
+      // 手机号格式  1开头 第2位 3-9 最后有9位数字即可
+      if (!/^1[3-9]\d{9}$/.test(value)) return callback(new Error('手机号格式不对'))
+      callback()
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 1.对整个表单进行校验
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          console.log('success')
+          // 2.校验成功发送登录请求
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // console.log(res.data)
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped lang='less'>
+.container{
+  width: 100%;
+  height: 100%;
+  position:absolute;
+   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+    }
+  }
+}
 
 </style>
